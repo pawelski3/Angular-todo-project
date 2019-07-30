822cbba0f965b541709cfdc70c59d2f56cb38f61
pawelski3 <pawelski3@o2.pl>
2019-07-30 20:25:44 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  newTask: string;
+  taskList: Array<string> = [];
+  taskDone: Array<string> = [];
+
+  add() {
+    this.taskList.push(this.newTask);
+    this.newTask = "";
+    console.log(this.taskList);
+
+  }
+
+  remove(task: string) {
+    this.taskList = this.taskList.filter(e => e !== task);
+  }
+
+  done(task: string) {
+    this.taskDone.push(task);
+    this.remove(task);
+    console.log(this.taskList)
+  }
+
+
+
 }
